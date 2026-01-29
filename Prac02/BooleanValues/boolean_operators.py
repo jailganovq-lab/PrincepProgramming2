9913c8b96d3c18909750a25ec6fd5ed715ff12c8
Qanat Jailganoc <jailganovq@gmail.com>
2026-01-29 13:19:35 +0500

    Add boolean operators examples

---
+++ Prac02/BooleanValues/boolean_operators.py
@@ -0,0 +1,23 @@
+# 1 and
+a = True
+b = False
+print(a and b)   # False
+
+# 2 or
+x = False
+y = False
+print(x or y)    # False
+
+# 3 not
+is_open = True
+print(not is_open)   # False
+
+# 4
+age = 20
+has_id = True
+print(age >= 18 and has_id)   # True
+
+# 5
+rain = False
+snow = True
+print(rain or snow)   # True
