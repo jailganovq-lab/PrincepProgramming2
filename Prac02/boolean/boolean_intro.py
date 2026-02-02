713f57dda2a7f8fd1abf6bcc75fc702035132f19
Qanat Jailganoc <jailganovq@gmail.com>
2026-02-02 21:31:48 +0500

    codes are coming

---
+++ Prac02/boolean/boolean_intro.py
@@ -0,0 +1,36 @@
+# 1
+is_student = True
+is_working = False
+
+print(is_student)
+print(is_working)
+
+# 2
+has_ticket = True
+
+if has_ticket:
+    print("You can enter")
+else:
+    print("You cannot enter")
+
+
+# 3
+a = 10
+b = 5
+
+result = a > b
+print(result)  # True
+
+# 4
+x = 7
+y = 7
+
+print(x == y)   # True
+print(x != y)   # False
+
+# 5
+is_on = False
+print(is_on)
+
+is_on = True
+print(is_on)
