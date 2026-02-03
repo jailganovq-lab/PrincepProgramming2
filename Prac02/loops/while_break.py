9b9c785d10ca4b2f566989b774ef813a5525bbd4
Qanat Jailganoc <jailganovq@gmail.com>
2026-02-03 11:06:38 +0500

    understanding of mean break

---
+++ Prac02/loops/while_break.py
@@ -0,0 +1,25 @@
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+
+
+i = 1
+
+while True:
+    if i == 5:
+        break
+    print(i)
+    i += 1
+
+
+i = 1
+
+while i <= 100:
+    if i % 7 == 0:
+        print(i)
+        break
+    i += 1
