fccee433e0003326a5678c33ba82e6bfb219fdee
Qanat Jailganoc <jailganovq@gmail.com>
2026-02-03 11:00:48 +0500

    understanding meaning of continue

---
+++ Prac02/loops/for_continue.py
@@ -0,0 +1,18 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    print(i)
+
