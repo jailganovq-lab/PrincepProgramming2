87919e98763adb24c8046ca087edec16d650269c
Qanat Jailganoc <jailganovq@gmail.com>
2026-02-03 10:58:39 +0500

    understanding meaning of break

---
+++ Prac02/loops/for_break.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+  
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+
