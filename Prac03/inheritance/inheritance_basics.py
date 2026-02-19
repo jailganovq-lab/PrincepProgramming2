f524ac2bb7352433ce7bbf9c869133fd67fa6a99
Qanat Jailganoc <jailganovq@gmail.com>
2026-02-19 09:48:38 +0500

    inher basics in work

---
+++ Prac03/inheritance/inheritance_basics.py
@@ -0,0 +1,39 @@
+class Animal:
+    def speak(self):
+        print("Жануар дыбыс шығарады")
+
+class Dog(Animal):
+    pass
+
+d = Dog()
+d.speak()
+
+class Animal:
+    def speak(self):
+        print("Жануар дыбыс шығарады")
+
+class Cat(Animal):
+    def speak(self):
+        print("Мысық мияулайды")
+
+c = Cat()
+c.speak()
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def info(self):
+        print(f"Аты: {self.name}")
+
+class Student(Person):
+    def __init__(self, name, grade):
+        super().__init__(name)
+        self.grade = grade
+
+    def info(self):
+        super().info()
+        print(f"Бағасы: {self.grade}")
+
+s = Student("Айбек", 90)
+s.info()
