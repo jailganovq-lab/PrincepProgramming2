a3d68c5820d1e5f009acd04180766c3c6a19c256
Qanat Jailganoc <jailganovq@gmail.com>
2026-02-19 09:31:05 +0500

    basics of lambda are working

---
+++ Prac03/lambda/lambda_basics.py
@@ -0,0 +1,19 @@
+square = lambda x: x ** 2
+
+print(square(5))  # 25
+
+add = lambda a, b: a + b
+
+result = add(10, 15)
+print("Қосынды:", result)
+
+students = [
+    ("Айжан", 85),
+    ("Ержан", 92),
+    ("Дана", 78)
+]
+
+# Бағасы бойынша сұрыптау
+sorted_students = sorted(students, key=lambda student: student[1])
+
+print(sorted_students)
