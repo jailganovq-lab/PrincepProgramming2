847a8165c6c9a7809ed0dbd2dc05d044bb6507b2
Qanat Jailganoc <jailganovq@gmail.com>
2026-02-26 16:38:27 +0500

    are done

--- Prac04/generators/ex4.py
+++ Prac04/generators/ex4.py
@@ -1,7 +1,7 @@
 def squares(a, b):
     for i in range(a, b + 1):
         yield i**2
-n=int(input())
-b=int(input())
+n,b=map(int, input().split())
+
 for value in squares(n, b):
-    print(value, end=" ")
+    print(value)
