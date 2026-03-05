6039e61d41a6946fb196f2a6cac8cac93baf6076
Qanat Jailganoc <jailganovq@gmail.com>
2026-03-05 13:14:08 +0500

    updated by version teacher

--- Prac05/receipt_parser.py
+++ Prac05/receipt_parser.py
@@ -1,51 +1,45 @@
 import re
 import json
 
-# read receipt
-with open("raw.txt", "r", encoding="utf-8") as file:
-    text = file.read()
+with open("raw.txt", "r", encoding="utf-8") as f:
+    text = f.read()
 
 
-# 1. Extract prices (only correct money format)
-price_pattern = r"\d{1,3}(?: \d{3})*,\d{2}"
-prices = re.findall(price_pattern, text)
+# product name + total price
+pattern = r"\d+\.\s*\n(.+?)\n.*?\n([\d\s]+,\d{2})"
 
-# convert prices to float
-prices_clean = []
-for p in prices:
-    clean = p.replace(" ", "").replace(",", ".")
-    try:
-        prices_clean.append(float(clean))
-    except ValueError:
-        pass
+matches = re.findall(pattern, text, re.DOTALL)
 
+products = []
 
-# 2. Extract product names
-product_pattern = r"\d+\.\s*\n(.+)"
-products = re.findall(product_pattern, text)
+for name, total in matches:
+    price = float(total.replace(" ", "").replace(",", "."))
+    
+    products.append({
+        "name": name.strip(),
+        "total_price": price
+    })
 
 
-# 3. Calculate total
-total_calculated = sum(prices_clean)
+# calculate total
+calculated_total = sum(p["total_price"] for p in products)
 
 
-# 4. Extract date and time
+# datetime
 datetime_pattern = r"\d{2}\.\d{2}\.\d{4}\s\d{2}:\d{2}:\d{2}"
 datetime_match = re.search(datetime_pattern, text)
 datetime = datetime_match.group() if datetime_match else None
 
 
-# 5. Extract payment method
+# payment method
 payment_pattern = r"(Банковская карта|Наличные)"
 payment_match = re.search(payment_pattern, text)
 payment_method = payment_match.group() if payment_match else None
 
 
-# 6. Structured output
 result = {
     "products": products,
-    "prices": prices_clean,
-    "calculated_total": total_calculated,
+    "calculated_total": calculated_total,
     "payment_method": payment_method,
     "datetime": datetime
 }
