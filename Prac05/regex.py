d16d34773ecec422c015df3df6fd4f419ab66fa6
Qanat Jailganoc <jailganovq@gmail.com>
2026-03-19 09:26:07 +0500

    we are done

--- Prac05/regex.py
+++ Prac05/regex.py
@@ -10,7 +10,7 @@ print()
 
 # 2. Match a string that has 'a' followed by two to three 'b'
 print("2 Task")
-text2 = "abb"
+text2 = "ab"
 pattern2 = r"ab{2,3}"
 print("Match:", bool(re.fullmatch(pattern2, text2)))
 print()
@@ -18,7 +18,7 @@ print()
 
 # 3. Find sequences of lowercase letters joined with underscore
 print("3 Task")
-text3 = "hello_world test_case example"
+text3 = "hello_world test case example"
 pattern3 = r"[a-z]+_[a-z]+"
 print("Result:", re.findall(pattern3, text3))
 print()
@@ -26,7 +26,7 @@ print()
 
 # 4. Find sequences of one uppercase letter followed by lowercase letters
 print("4 Task")
-text4 = "Hello World Python Programming"
+text4 = "Hello World Python programming"
 pattern4 = r"[A-Z][a-z]+"
 print("Result:", re.findall(pattern4, text4))
 print()
@@ -34,7 +34,7 @@ print()
 
 # 5. Match string that has 'a' followed by anything ending with 'b'
 print("5 Task")
-text5 = "a123b"
+text5 = "a123bc"
 pattern5 = r"a.*b"
 print("Match:", bool(re.fullmatch(pattern5, text5)))
 print()
