201674099c2dd913f6fa41d394a4a08dedf69256
Qanat Jailganoc <jailganovq@gmail.com>
2026-04-28 01:03:07 +0500

    tsis are done

---
+++ Endterm/TSIS4/config.py
@@ -0,0 +1,10 @@
+# PostgreSQL connection settings
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "phonebook_db",
+    "user": "qanatjailganov",
+    "password": "12345678",
+    "port": 5432
+}
+
+
